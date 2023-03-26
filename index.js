810d33b5ffa31ec9eae4ba55764fd622fde43d03
Sancho Godinho <sanchogodinho98@gmail.com>
2023-03-27 00:11:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,10 @@
-const express = require('express');
-
-const fs = require('fs');
-
-const cors = require('cors');
-
-const app = express();
-
+const express=require("express");
+const cors=require("cors");
+const app=express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-
-  fs.readFile('api.json', 'utf8', (err, data) => {
-
-    if (err) {
-
-      console.error(err);
-
-      res.status(500).send('Internal Server Error');
-
-      return;
-
-    }
-
-    const dataObj = JSON.parse(data);
-
-    res.json(dataObj);
-
-  });
-
+app.get('/', (req, res)=>{
+    res.send("Hello Dev Nasfam!");
 });
 
-const server = app.listen(3000, () => {
-
-  console.log(`Server listening on port ${server.address().port}`);
-
-});
-
-
-
+app.listen(8080);
